3691affd9b4f768431101b389ca803cd0ff64d3b
christopher <christopher.hirudayasamy@syncfusion.com>
2020-11-10 12:04:05 +0530

    GitHubCIProcess

--- gulpfile.js
+++ gulpfile.js
@@ -62,7 +62,7 @@ gulp.task('ship-to-gitlab', function (done) {
         shelljs.exec('git add .');
         shelljs.exec('git pull');
         shelljs.exec('git commit -m \"source updation from github repo \" --no-verify');
-        shelljs.exec('git push');
+        shelljs.exec('git push --force');
         shelljs.cd('../../');
     }
 })
