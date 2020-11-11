ced6c8ffe062a9fc01f2af483ddc5f222185367f
christopher <christopher.hirudayasamy@syncfusion.com>
2020-11-11 19:14:26 +0530

    source updation

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ gulp.task('ship-to-gitlab', function (done) {
 
         for (var changedFileName of changedFileNames.split(',')) {
 
-            if (changedFileName !== null && changedFileName !== '' && changedFileName !== '.gitignore') {
+            if (changedFileName !== null && changedFileName !== '' && changedFileName !== '.gitignore' && !changedFileName.includes('.gitlab')) {
 
                 if (fs.existsSync('../asp.net-core-docs/' + changedFileName)) {
                     // It will update the modified files
